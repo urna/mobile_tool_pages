bda954f16219feeead11852125040ae4b3ad03da
Wang HaiJun <armstrong.wang@gmail.com>
2014-06-25 08:54:11 +0800

    init

---
+++ tool_files/16833658.js
@@ -0,0 +1,10 @@
+document.write ('<a href="http://www.51.la/?16833658" target="_blank" title="&#x35;&#x31;&#x2E;&#x6C;&#x61;&#x20;&#x4E13;&#x4E1A;&#x3001;&#x514D;&#x8D39;&#x3001;&#x5F3A;&#x5065;&#x7684;&#x8BBF;&#x95EE;&#x7EDF;&#x8BA1;">&#x7F51;&#x7AD9;&#x7EDF;&#x8BA1;</a>\n');
+var a3658tf="51la";var a3658pu="";var a3658pf="51la";var a3658su=window.location;var a3658sf=document.referrer;var a3658of="";var a3658op="";var a3658ops=1;var a3658ot=1;var a3658d=new Date();var a3658color="";if (navigator.appName=="Netscape"){a3658color=screen.pixelDepth;} else {a3658color=screen.colorDepth;}
+try{a3658tf=top.document.referrer;}catch(e){}
+try{a3658pu =window.parent.location;}catch(e){}
+try{a3658pf=window.parent.document.referrer;}catch(e){}
+try{a3658ops=document.cookie.match(new RegExp("(^| )AJSTAT_ok_pages=([^;]*)(;|$)"));a3658ops=(a3658ops==null)?1: (parseInt(unescape((a3658ops)[2]))+1);var a3658oe =new Date();a3658oe.setTime(a3658oe.getTime()+60*60*1000);document.cookie="AJSTAT_ok_pages="+a3658ops+ ";path=/;expires="+a3658oe.toGMTString();a3658ot=document.cookie.match(new RegExp("(^| )AJSTAT_ok_times=([^;]*)(;|$)"));if(a3658ot==null){a3658ot=1;}else{a3658ot=parseInt(unescape((a3658ot)[2])); a3658ot=(a3658ops==1)?(a3658ot+1):(a3658ot);}a3658oe.setTime(a3658oe.getTime()+365*24*60*60*1000);document.cookie="AJSTAT_ok_times="+a3658ot+";path=/;expires="+a3658oe.toGMTString();}catch(e){}
+try{if(document.cookie==""){a3658ops=-1;a3658ot=-1;}}catch(e){}
+a3658of=a3658sf;if(a3658pf!=="51la"){a3658of=a3658pf;}if(a3658tf!=="51la"){a3658of=a3658tf;}a3658op=a3658pu;try{lainframe}catch(e){a3658op=a3658su;}
+a3658src='http://web2.51.la:82/go.asp?svid=20&id=16833658&tpages='+a3658ops+'&ttimes='+a3658ot+'&tzone='+(0-a3658d.getTimezoneOffset()/60)+'&tcolor='+a3658color+'&sSize='+screen.width+','+screen.height+'&referrer='+escape(a3658of)+'&vpage='+escape(a3658op)+'&vvtime='+a3658d.getTime();
+setTimeout('a3658img = new Image;a3658img.src=a3658src;',0);
